1c2b678cc79f175815ab95817b0c3f0fe5d6b72a
HATSUNE, Akira <hatsune.a@nifty.com>
2015-06-06 18:11:30 +0900

    Sample 03

---
+++ HandsOn03/HandsOn/Converter/WaveConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace HandsOn.Converter
+{
+    public sealed class WaveConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is HandsOn.Models.KinectModel.ResultState)
+            {
+                if ((HandsOn.Models.KinectModel.ResultState)value == HandsOn.Models.KinectModel.ResultState.Lock)
+                {
+                    return new Uri("ms-appx:///Assets/17goo.wav");
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
